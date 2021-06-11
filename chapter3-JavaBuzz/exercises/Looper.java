b39d53e4e9f38688575a096c73fa199d268f11ba
Adam Jonas <adamjonas25@gmail.com>
2021-06-11 09:20:29 +0100

    Define Java Buzz print out 1-100

---
+++ chapter3-JavaBuzz/exercises/Looper.java
@@ -0,0 +1,10 @@
+public class Looper {
+    private static int[] args;
+
+    public static void main (int[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
